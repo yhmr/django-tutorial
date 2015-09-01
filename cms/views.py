103b089cbd5e9d830f84394d2f38a7b1025c32dc
yuhomori <morimo07@outlook.jp>
2015-09-01 15:15:19 +0900

    Finish Django Introduction Chapter 4
    [Qiita page](http://qiita.com/kaki_k/items/6e17597804437ef170ae)

---
+++ cms/views.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from cms.forms import BookForm
+from cms.models import Book
+
+def book_list(request):
+    '''書籍の一覧'''
+#    return HttpResponse(u'書籍の一覧')
+    books = Book.objects.all().order_by('id')
+    return render_to_response('cms/book_list.html',  # 使用するテンプレート
+                              {'books': books},       # テンプレートに渡すデータ
+                              context_instance=RequestContext(request))  # その他標準のコンテキスト
+
+def book_edit(request, book_id=None):
+    '''書籍の編集'''
+#     return HttpResponse(u'書籍の編集')
+    if book_id:   # book_id が指定されている (修正時)
+        book = get_object_or_404(Book, pk=book_id)
+    else:         # book_id が指定されていない (追加時)
+        book = Book()
+
+    if request.method == 'POST':
+        form = BookForm(request.POST, instance=book)  # POST された request データからフォームを作成
+        if form.is_valid():    # フォームのバリデーション
+            book = form.save(commit=False)
+            book.save()
+            return redirect('cms:book_list')
+    else:    # GET の時
+        form = BookForm(instance=book)  # book インスタンスからフォームを作成
+
+    return render_to_response('cms/book_edit.html',
+                              dict(form=form, book_id=book_id),
+                              context_instance=RequestContext(request))
+
+def book_del(request, book_id):
+    '''書籍の削除'''
+#     return HttpResponse(u'書籍の削除')
+    book = get_object_or_404(Book, pk=book_id)
+    book.delete()
+    return redirect('cms:book_list')
